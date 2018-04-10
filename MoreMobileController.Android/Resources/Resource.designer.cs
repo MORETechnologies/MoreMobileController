622b217115fd3aadf84c468b379604034d880595
Hieu Truong <hieu@tnhlabs.com>
2018-04-10 17:08:29 -0500

    Add animator for pressing buttons

--- MoreMobileController.Android/Resources/Resource.designer.cs
+++ MoreMobileController.Android/Resources/Resource.designer.cs
@@ -28,6 +28,22 @@ namespace MoreMobileController.Android
 		{
 		}
 		
+		public partial class Animator
+		{
+			
+			// aapt resource value: 0x7f050000
+			public const int control_button = 2131034112;
+			
+			static Animator()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Animator()
+			{
+			}
+		}
+		
 		public partial class Attribute
 		{
 			
@@ -69,41 +85,41 @@ namespace MoreMobileController.Android
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f070007
-			public const int backwardButton = 2131165191;
+			// aapt resource value: 0x7f080007
+			public const int backwardButton = 2131230727;
 			
-			// aapt resource value: 0x7f070004
-			public const int connectButton = 2131165188;
+			// aapt resource value: 0x7f080004
+			public const int connectButton = 2131230724;
 			
-			// aapt resource value: 0x7f07000b
-			public const int forwardButton = 2131165195;
+			// aapt resource value: 0x7f08000b
+			public const int forwardButton = 2131230731;
 			
-			// aapt resource value: 0x7f070000
-			public const int hostEditText = 2131165184;
+			// aapt resource value: 0x7f080000
+			public const int hostEditText = 2131230720;
 			
-			// aapt resource value: 0x7f070009
-			public const int leftButton = 2131165193;
+			// aapt resource value: 0x7f080009
+			public const int leftButton = 2131230729;
 			
-			// aapt resource value: 0x7f070001
-			public const int linearLayout1 = 2131165185;
+			// aapt resource value: 0x7f080001
+			public const int linearLayout1 = 2131230721;
 			
-			// aapt resource value: 0x7f070008
-			public const int modeButton = 2131165192;
+			// aapt resource value: 0x7f080008
+			public const int modeButton = 2131230728;
 			
-			// aapt resource value: 0x7f070003
-			public const int portEditText = 2131165187;
+			// aapt resource value: 0x7f080003
+			public const int portEditText = 2131230723;
 			
-			// aapt resource value: 0x7f070006
-			public const int relativeLayout1 = 2131165190;
+			// aapt resource value: 0x7f080006
+			public const int relativeLayout1 = 2131230726;
 			
-			// aapt resource value: 0x7f07000a
-			public const int rightButton = 2131165194;
+			// aapt resource value: 0x7f08000a
+			public const int rightButton = 2131230730;
 			
-			// aapt resource value: 0x7f070002
-			public const int space1 = 2131165186;
+			// aapt resource value: 0x7f080002
+			public const int space1 = 2131230722;
 			
-			// aapt resource value: 0x7f070005
-			public const int statusTextView = 2131165189;
+			// aapt resource value: 0x7f080005
+			public const int statusTextView = 2131230725;
 			
 			static Id()
 			{
@@ -153,11 +169,11 @@ namespace MoreMobileController.Android
 		public partial class String
 		{
 			
-			// aapt resource value: 0x7f050001
-			public const int app_name = 2131034113;
+			// aapt resource value: 0x7f060001
+			public const int app_name = 2131099649;
 			
-			// aapt resource value: 0x7f050000
-			public const int hello = 2131034112;
+			// aapt resource value: 0x7f060000
+			public const int hello = 2131099648;
 			
 			static String()
 			{
@@ -172,8 +188,8 @@ namespace MoreMobileController.Android
 		public partial class Style
 		{
 			
-			// aapt resource value: 0x7f060000
-			public const int BlueButton = 2131099648;
+			// aapt resource value: 0x7f070000
+			public const int blue_button = 2131165184;
 			
 			static Style()
 			{
