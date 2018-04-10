594b5cb7807b9af34995b5c9cc0a42ae9382d137
Hieu Truong <hieu@tnhlabs.com>
2018-04-10 18:12:13 -0500

    Integrate buttons with viewmodel

--- MoreMobileController.Android/ControlActivity.cs
+++ MoreMobileController.Android/ControlActivity.cs
@@ -3,6 +3,7 @@ using Android.OS;
 using Android.Util;
 using Android.Views;
 using Android.Widget;
+using MoreMobileController.Core;
 
 namespace MoreMobileController.Android
 {
@@ -14,12 +15,15 @@ namespace MoreMobileController.Android
         ImageButton rightButton;
         ImageButton backwardButton;
         Button modeButton;
+        ControlViewModel viewModel;
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
             SetContentView(Resource.Layout.Control);
+
+            viewModel = ((MainApplication)Application).ConnectViewModel.ControlViewModel;
 
             forwardButton = FindViewById<ImageButton>(Resource.Id.forwardButton);
             leftButton = FindViewById<ImageButton>(Resource.Id.leftButton);
@@ -36,12 +40,54 @@ namespace MoreMobileController.Android
             MakeSquare(rightButton, size);
             MakeSquare(backwardButton, size);
             MakeSquare(modeButton, size);
+
+            forwardButton.Touch += (sender, e) => {
+                OnControlPressed(ControlButton.Forward, e);
+            };
+
+            leftButton.Touch += (sender, e) => {
+                OnControlPressed(ControlButton.Left, e);
+            };
+
+            rightButton.Touch += (sender, e) => {
+                OnControlPressed(ControlButton.Right, e);
+            };
+
+            backwardButton.Touch += (sender, e) => {
+                OnControlPressed(ControlButton.Backward, e);
+            };
+
+            modeButton.Click += (sender, e) => {
+                viewModel.OnButtonPressed(ControlButton.Mode);
+            };
+
+            viewModel.ModeTextChanged += (sender, e) => {
+                modeButton.Text = e;
+            };
+        }
+
+        public override void OnBackPressed()
+        {
+            base.OnBackPressed();
+
+            viewModel.OnBackPressed();
         }
 
         private void MakeSquare(View view, int size)
         {
             view.LayoutParameters.Width = size;
             view.LayoutParameters.Height = size;
+        }
+
+        private void OnControlPressed(ControlButton button, View.TouchEventArgs e)
+        {
+            if (e.Event.Action == MotionEventActions.Down) {
+                viewModel.OnButtonPressed(button);
+            } else if (e.Event.Action == MotionEventActions.Up) {
+                viewModel.OnButtonReleased();
+            }
+
+            e.Handled = false;
         }
     }
 }
